2a1635f0482f0c883f1f6f8231c49a62bd0cafaf
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-07 14:41:39 +0530

    Created NavBar

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from 'react'
 
 function App() {
   return (
-    <h1 className="text-3xl font-semibold hover:font-extrabold">
-      Hello world!
-    </h1>
+    <div className='min-h-screen bg-slate-950'>
+     <nav className='w-full h-14 bg-indigo-200 flex justify-between px-4 md:px-4 items-center'>
+      <div className='text-2xl text-indigo-700 font-bold'>
+        Bitcoin
+      </div>
+      <ul className='md:flex hidden font-semibold'>
+        <li className=' mx-[10px] cursor-pointer'>Home</li>
+        <li className=' mx-[10px] cursor-pointer'>Contact Us</li>
+        <li className=' mx-[10px] cursor-pointer'>About</li>
+      </ul>
+      <div className='hidden md:block bg-indigo-700 text-white rounded font-bold cursor-pointer '>Login/SignUp</div>
+      <div className='md:hidden'>
+        <a className=' text-4xl' href='#'>&#8801;</a>
+      </div>
+     </nav>
+    </div>
   )
 }
 
