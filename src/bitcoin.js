265a684369ebd2b8f6e01ebc10c889fa36b486c9
Shivansh Singh <singhshivansh12may@gmail.com>
2024-01-08 17:57:18 +0530

    Added cards

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -37,7 +37,7 @@ function Bitcoin() {
             </div>
            <div className='hidden md:flex md:justify-center md:space-x-3 md:items-center md:mt-9'>
            <div className="w-12 h-12 bg-black rounded-full flex items-center justify-center">
-                <svg className="w-6 h-6 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <svg className="w-6 h-6 text-white cursor-pointer" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 3l14 9-14 9V3z"></path>
                 </svg>
                 </div>
@@ -45,30 +45,38 @@ function Bitcoin() {
                 What is bitcoin ?
              </div>
              </div>
-             <div className='w-full h-[700px] bg-black bg-opacity-75 mt-[150px]'>
+             <div className='w-full md:h-[700px] h-[800px] bg-black bg-opacity-85 mt-[150px]'>
                     <div className='flex text-yellow-50 text-2xl py-[180px] justify-center'>
                         Get a quick overview for
                     </div>
 
-                    <div className=' flex justify-center space-x-5 mt-0'>
-                        <div className='w-[250px] h-[220px] bg-black bg-opacity-90 flex flex-col justify-center items-center space-y-3'>
+                    <div className=' flex flex-col md:flex-row  justify-center items-center md:space-x-5 mt-0 space-y-2'>
+                        <div className='md:w-[250px] md:h-[220px] w-[300px] h-[100px] bg-black bg-opacity-90 flex md:flex-col flex-row justify-center items-center space-y-3 space-x-4'>
                         <img src='https://icons.veryicon.com/png/o/miscellaneous/44-2px-linear/people-17.png' className='w-[50px] space-y-5'></img>
+                        <div className='flex flex-col space-x-3'>
                         <span className='text-white font-semibold text-xl'>Individuals</span>
-                        <span className='text-gray-700 text-[15px]'>Learn More</span>
+                        <span className='text-gray-700 text-[15px] cursor-pointer'>Learn More</span>
                         </div>
-                        <div className='w-[250px] h-[220px] bg-black bg-opacity-90 flex flex-col justify-center items-center space-y-3'>
+                        </div>
+                        <div className='md:w-[250px] md:h-[220px] w-[300px] h-[100px] bg-black bg-opacity-90 flex md:flex-col flex-row justify-center items-center space-y-3 space-x-4'>
                         <img src='https://cdn3.vectorstock.com/i/1000x1000/16/57/shopping-bag-orange-icon-on-black-vector-13391657.jpg' className='w-[50px] space-y-5'></img>
-                        <span className='text-white font-semibold text-xl'>Individuals</span>
-                        <span className='text-gray-700 text-[15px]'>Learn More</span>
+                        <div className='flex flex-col space-x-3'>
+                        <span className='text-white font-semibold text-xl'>Business</span>
+                        <span className='text-gray-700 text-[15px] cursor-pointer'>Learn More</span>
                         </div>
-                        <div className='w-[250px] h-[220px] bg-black bg-opacity-90 flex flex-col justify-center items-center space-y-3'>
+                        </div>
+                        <div className='md:w-[250px] md:h-[220px] w-[300px] h-[100px] bg-black bg-opacity-90 flex md:flex-col flex-row justify-center items-center space-y-3 space-x-4'>
                         <img src='https://icones.pro/wp-content/uploads/2022/03/icone-d-ordinateur-portable-orange.png' className='w-[50px] space-y-5'></img>
-                        <span className='text-white font-semibold text-xl'>Individuals</span>
-                        <span className='text-gray-700 text-[15px]'>Learn More</span>
+                        <div className='flex flex-col space-x-3'>
+                        <span className='text-white font-semibold text-xl'>Developers</span>
+                        <span className='text-gray-700 text-[15px] cursor-pointer'>Learn More</span>
+                        </div>
                         </div>
                     </div>
                     </div>
-
+                <div className=' w-full h-full mt-5 bg-white'>
+                 <div>Get St</div>
+                </div>
     </div>
   )
 }
